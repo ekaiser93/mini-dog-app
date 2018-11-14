4d63444f704c356c21fc79f84e5f2ab14e6cd3b0
ekaiser93 <e.kaiser.93@gmail.com>
2018-11-14 13:04:18 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app"><img width="25%" src="./assets/logo.png" /> <HelloWorld /></div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
+<style>
+img {
+  max-width: 100%;
+}
+
+h1 {
+  padding-bottom: 15px;
+}
+
+.dogs-layout {
+  width: 100%;
+  background: #fff center repeat;
+  background-image: url("https://github.com/VueVixens/projects/blob/master/petshop/images/bg3.jpg?raw=true");
+}
+
+.dogs-overlay {
+  width: 100%;
+  padding: 20px;
+  background-color: #fff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+@media (max-width: 768px) {
+  .dogs-overlay {
+    margin: 0;
+  }
+}
+
+.dog-card {
+  width: 100%;
+  max-width: 600px;
+}
+</style>
